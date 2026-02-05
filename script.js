a704d3d80ed889b0516a23f15322ceb630bb0d48
koonicole <nicolekookim@gmail.com>
2026-02-04 20:24:20 -0600

    Add event listeners for circle interactions

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const circulo = document.getElementById("circulo");
 
+circulo.addEventListener("mouseover", () => {
+  circulo.style.transform = "scale(1.5)";
+});
+
+circulo.addEventListener("mouseout", () => {
+  circulo.style.transform = "scale(1)";
+});
+
+circulo.addEventListener("click", () => {
+  circulo.style.backgroundColor = "hotpink";
+});
